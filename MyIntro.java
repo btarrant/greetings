54bf4183c798b9dbe59a00a9c120b49dd721653e
Brandon Tarrant <brandontarrant04@gmail.com>
2022-04-30 17:24:39 -0400

    first commit

---
+++ MyIntro.java
@@ -0,0 +1,7 @@
+public class MyIntro {
+    public static void main(String[] args) {
+        System.out.println("My name is Brandon Tarrant");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is New York, NY");
+    }
+}
